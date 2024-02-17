3f7369fb1cb6d3677e5196f7f712789a125800e7
sulthanrizal <surie0312@gmail.com>
2024-02-17 20:05:46 +0700

    create testing for front end

--- phonebook-client/src/App.test.js
+++ phonebook-client/src/App.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-
 test('renders link', () => {
   render(<App />);
   const linkElement = screen.getByRole('link');
