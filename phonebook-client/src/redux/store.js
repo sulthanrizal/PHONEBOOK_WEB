948f488f57e3137b66c4c79bfc0d1588425edb9f
sulthanrizal <surie0312@gmail.com>
2024-02-17 15:53:15 +0700

    create connection redux and react

--- phonebook-client/src/redux/store.js
+++ phonebook-client/src/redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import phonebookReducer from './phonebook';
 
+
 export const store = configureStore({
     reducer: {
         phonebook: phonebookReducer,
